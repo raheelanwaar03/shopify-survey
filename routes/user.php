abb39c63724e03058ea56649e9ee47a40d42f63c
raheel0316 <raheelanwaar0316@gmail.com>
2024-04-30 20:36:45 -0700

    new Changes

--- routes/user.php
+++ routes/user.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\user\UserDashboardController;
 use App\Http\Controllers\user\WithdrawController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('User.')->prefix('User')->middleware('auth', 'user')->group(function () {
+Route::name('User.')->prefix('User')->middleware('auth', 'user', 'fees')->group(function () {
     Route::get('/Dashboard', [UserDashboardController::class, 'index'])->name('Dashboard');
     // Withdraw Requsts
     Route::get('Apply/Withdraw', [WithdrawController::class, 'apply'])->name('Apply.Withdraw');
