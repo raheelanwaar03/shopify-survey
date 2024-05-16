d053702d8b2a36966964f024b85480ae08482107
raheel0316 <raheelanwaar0316@gmail.com>
2024-05-16 14:08:55 -0700

    card

--- resources/views/user/tasks/extra.blade.php
+++ resources/views/user/tasks/extra.blade.php
@@ -19,14 +19,12 @@
             </div>
             <div class="row">
                 @foreach ($task as $item)
-                    <div class="col-md-4">
-                        <div class="card" style="width: 18rem;">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $item->title }}</h5>
-                                <p class="card-text">{{ $item->des }}</p>
-                                <a href="{{ route('User.Get.Extra.Task.Reward', $item->id) }}" class="btn btn-success"
-                                    onclick="window.open('{{ $item->link }}', '_blank')">Get Reward</a>
-                            </div>
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $item->title }}</h5>
+                            <p class="card-text">{{ $item->des }}</p>
+                            <a href="{{ route('User.Get.Extra.Task.Reward', $item->id) }}" class="btn btn-success"
+                                onclick="window.open('{{ $item->link }}', '_blank')">Get Reward</a>
                         </div>
                     </div>
                 @endforeach
